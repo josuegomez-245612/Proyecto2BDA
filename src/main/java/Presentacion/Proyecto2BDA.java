68e339d5d3570f1e4795c378793e7214ba50c340
josuegomez-245612 <josue.gomez.personal@gmail.com>
2024-03-18 14:15:25 -0700

    Creacion inicial de pantalla Principal

---
+++ src/main/java/Presentacion/Proyecto2BDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Presentacion;
+
+/**
+ *
+ * @author RAUL EDUARDO GOMEZ
+ */
+public class Proyecto2BDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
