e3c4edaed73fdd6a239f76fa6af3eb46aab80edc
GitHElias <jorge.elias@potros.itson.edu.mx>
2024-04-05 17:28:10 -0700

    Update Nombre Variables ConsultarPlacas.java

--- src/main/java/Presentacion/ConsultarPlacas.java
+++ src/main/java/Presentacion/ConsultarPlacas.java
@@ -27,25 +27,24 @@ public class ConsultarPlacas extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        imagen = new javax.swing.JLabel();
+        headerImgLabel = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        tituloLabel = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        personaTable = new javax.swing.JTable();
+        curpTxtField = new javax.swing.JTextField();
+        nombreTxtField = new javax.swing.JTextField();
+        fechaNacimientoTxtField = new javax.swing.JTextField();
+        curpLabel = new javax.swing.JLabel();
+        nombreLabel = new javax.swing.JLabel();
+        fechaNacimientoLabel = new javax.swing.JLabel();
+        buscarPorLabel = new javax.swing.JLabel();
+        buscarBtn = new javax.swing.JButton();
+        consultarLabel = new javax.swing.JButton();
+        volverLabel = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(800, 900));
-        setPreferredSize(new java.awt.Dimension(800, 900));
         setResizable(false);
         setSize(new java.awt.Dimension(800, 900));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -56,21 +55,21 @@ public class ConsultarPlacas extends javax.swing.JFrame {
         jPanel1.setMinimumSize(new java.awt.Dimension(800, 130));
         jPanel1.setPreferredSize(new java.awt.Dimension(800, 130));
 
-        imagen.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/marcoPantallaPrincipal.png"))); // NOI18N
-        imagen.setText("a");
+        headerImgLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/marcoPantallaPrincipal.png"))); // NOI18N
+        headerImgLabel.setText("a");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(headerImgLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(headerImgLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
@@ -82,10 +81,10 @@ public class ConsultarPlacas extends javax.swing.JFrame {
         jPanel2.setMinimumSize(new java.awt.Dimension(800, 770));
         jPanel2.setPreferredSize(new java.awt.Dimension(800, 770));
 
-        jLabel1.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
-        jLabel1.setText("Seleccione a la persona a la cual se quiere consultar el historial de placas");
+        tituloLabel.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
+        tituloLabel.setText("Seleccione a la persona a la cual se quiere consultar el historial de placas");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        personaTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {"Jorge Elias", "JEIE46856161", "08/02/2024"},
                 {null, null, null},
@@ -110,45 +109,45 @@ public class ConsultarPlacas extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jTable1.getTableHeader().setReorderingAllowed(false);
-        jScrollPane1.setViewportView(jTable1);
-        if (jTable1.getColumnModel().getColumnCount() > 0) {
-            jTable1.getColumnModel().getColumn(0).setResizable(false);
-            jTable1.getColumnModel().getColumn(1).setResizable(false);
-            jTable1.getColumnModel().getColumn(2).setResizable(false);
+        personaTable.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(personaTable);
+        if (personaTable.getColumnModel().getColumnCount() > 0) {
+            personaTable.getColumnModel().getColumn(0).setResizable(false);
+            personaTable.getColumnModel().getColumn(1).setResizable(false);
+            personaTable.getColumnModel().getColumn(2).setResizable(false);
         }
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        curpTxtField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                curpTxtFieldActionPerformed(evt);
             }
         });
 
-        jLabel2.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
-        jLabel2.setText("CURP");
+        curpLabel.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
+        curpLabel.setText("CURP");
 
-        jLabel3.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
-        jLabel3.setText("Nombre");
+        nombreLabel.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
+        nombreLabel.setText("Nombre");
 
-        jLabel4.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
-        jLabel4.setText("Fecha de Nacimiento");
+        fechaNacimientoLabel.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
+        fechaNacimientoLabel.setText("Fecha de Nacimiento");
 
-        jLabel5.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
-        jLabel5.setText("Buscar por: ");
+        buscarPorLabel.setFont(new java.awt.Font("sansserif", 1, 12)); // NOI18N
+        buscarPorLabel.setText("Buscar por: ");
 
-        jButton1.setText("Buscar");
+        buscarBtn.setText("Buscar");
 
-        jButton2.setText("Consultar");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        consultarLabel.setText("Consultar");
+        consultarLabel.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                consultarLabelActionPerformed(evt);
             }
         });
 
-        jButton3.setText("Volver");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        volverLabel.setText("Volver");
+        volverLabel.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                volverLabelActionPerformed(evt);
             }
         });
 
@@ -161,27 +160,27 @@ public class ConsultarPlacas extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel5)))
+                            .addComponent(tituloLabel)
+                            .addComponent(buscarPorLabel)))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(357, 357, 357)
-                        .addComponent(jButton2))
+                        .addComponent(consultarLabel))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(14, 14, 14)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton3))
+                            .addComponent(curpLabel)
+                            .addComponent(curpTxtField, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(volverLabel))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3))
+                            .addComponent(nombreTxtField, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(nombreLabel))
                         .addGap(12, 12, 12)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel4)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(fechaNacimientoLabel)
+                            .addComponent(fechaNacimientoTxtField, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(50, 50, 50)
-                        .addComponent(jButton1))
+                        .addComponent(buscarBtn))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(177, 177, 177)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -191,26 +190,26 @@ public class ConsultarPlacas extends javax.swing.JFrame {
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(50, 50, 50)
-                .addComponent(jLabel1)
+                .addComponent(tituloLabel)
                 .addGap(32, 32, 32)
-                .addComponent(jLabel5)
+                .addComponent(buscarPorLabel)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel2))
+                    .addComponent(nombreLabel)
+                    .addComponent(fechaNacimientoLabel)
+                    .addComponent(curpLabel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                    .addComponent(curpTxtField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nombreTxtField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(fechaNacimientoTxtField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(buscarBtn))
                 .addGap(34, 34, 34)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButton2)
+                .addComponent(consultarLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton3)
+                .addComponent(volverLabel)
                 .addContainerGap(43, Short.MAX_VALUE))
         );
 
@@ -220,37 +219,37 @@ public class ConsultarPlacas extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void curpTxtFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_curpTxtFieldActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_curpTxtFieldActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void volverLabelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_volverLabelActionPerformed
         this.dispose();
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_volverLabelActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void consultarLabelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_consultarLabelActionPerformed
         this.setVisible(false);
         HistorialPlacas historialPlacas = new HistorialPlacas(this);
         historialPlacas.setVisible(true);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_consultarLabelActionPerformed
 
    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel imagen;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    private javax.swing.JButton buscarBtn;
+    private javax.swing.JLabel buscarPorLabel;
+    private javax.swing.JButton consultarLabel;
+    private javax.swing.JLabel curpLabel;
+    private javax.swing.JTextField curpTxtField;
+    private javax.swing.JLabel fechaNacimientoLabel;
+    private javax.swing.JTextField fechaNacimientoTxtField;
+    private javax.swing.JLabel headerImgLabel;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JLabel nombreLabel;
+    private javax.swing.JTextField nombreTxtField;
+    private javax.swing.JTable personaTable;
+    private javax.swing.JLabel tituloLabel;
+    private javax.swing.JButton volverLabel;
     // End of variables declaration//GEN-END:variables
 }
